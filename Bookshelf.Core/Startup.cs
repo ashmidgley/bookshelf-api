cf5d97b9eca93d659aca930c61807c6046c98b0a
Ash Midgley <ayelgdom@tutanota.com>
2020-02-23 18:55:08 +0000

    Update Bookshelf.Core to dotnet core 3.1.

--- Bookshelf.Core/Startup.cs
+++ Bookshelf.Core/Startup.cs
@@ -2,10 +2,10 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.IdentityModel.Tokens;
 
 namespace Bookshelf.Core
@@ -40,8 +40,8 @@ namespace Bookshelf.Core
                 };
             });
 
+            services.AddControllers();
             services.AddCors();
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
             
             services.AddTransient<IBookRepository, BookRepository>();
             services.AddTransient<ICategoryRepository, CategoryRepository>();
@@ -58,7 +58,7 @@ namespace Bookshelf.Core
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             using (var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
             {
@@ -71,15 +71,25 @@ namespace Bookshelf.Core
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
             app.UseAuthentication();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
             app.UseCors(builder =>
                 builder
                     .AllowAnyHeader()
                     .AllowAnyMethod()
                     .AllowAnyOrigin()
             );
-            app.UseHttpsRedirection();
-            app.UseMvc();
         }
     }
 }
