11f5c829d475c160908a43e4f680bb21f8b8b90f
Ash Midgley <ayelgdom@tutanota.com>
2020-03-22 15:47:44 +0000

    Add explicit tag to google api test.

--- Bookshelf.Tests/Helper/SearchHelperShould.cs
+++ Bookshelf.Tests/Helper/SearchHelperShould.cs
@@ -9,6 +9,7 @@ namespace Bookshelf.Tests
     public class SearchHelperShould
     {
         [Test]
+        [Explicit]
         public void SearchGoogleBooks()
         {
             var theMartian = new NewBookDto
@@ -27,7 +28,7 @@ namespace Bookshelf.Tests
             var config = A.Fake<IConfiguration>();
             A.CallTo(() => config["DefaultCover"]).Returns(defaultImage);
             A.CallTo(() => config["GoogleBooks:Url"]).Returns("https://www.googleapis.com/books/v1");
-            A.CallTo(() => config["GoogleBooks:Key"]).Returns("");
+            A.CallTo(() => config["GoogleBooks:Key"]).Returns("AIzaSyCXxqGUxHHIKTglmzBQKA9sLTVSV8Q6yJw");
 
             var searchHelper = new SearchHelper(config);
 
