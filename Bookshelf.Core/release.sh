a80a7595f72bb72e130899439d73e9f71a1f332f
Ash Midgley <ayelgdom@tutanota.com>
2020-04-10 17:45:29 +0100

    Add comments to release script.

--- Bookshelf.Core/release.sh
+++ Bookshelf.Core/release.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+echo "##### Starting database container"
 docker start bookshelf-mssql
+echo "##### Stopping API container"
 docker stop bookshelf-api
+echo "##### Removing old API container instance"
 docker rm bookshelf-api
+echo "##### Building new image"
 docker build -t bookshelf-api .
+echo "##### Starting new API container"
 docker run -d -p 5000:80 --name bookshelf-api --link bookshelf-mssql:bookshelf-mssql bookshelf-api
